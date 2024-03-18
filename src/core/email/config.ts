27e2bd3b3a5199f3610f75e77811d572a6c32637
GeorgeZhang2035 <goerge.zhang@silksoftware.com>
2024-03-18 16:08:32 +0800

    feat: email module complete

---
+++ src/core/email/config.ts
@@ -0,0 +1,10 @@
+export default {
+  alias: 'UAVN admin',
+  host: 'smtp.163.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'z2530056984@qq.com',
+    pass: 'TZSRWOQCUXJGXYNF',
+  },
+};
--- src/core/email/email.service.ts
+++ src/core/email/email.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { createTransport } from 'nodemailer';
+import EMAIL from './config';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private transporter = null;
+  constructor() {
+    this.transporter = createTransport(EMAIL);
+  }
+
+  sendEmail(email: string, text: string, subject: string, html: unknown) {
+    
+  }
+}
