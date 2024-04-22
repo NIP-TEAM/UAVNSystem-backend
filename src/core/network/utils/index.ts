1253405e1a3def939f9da4a2cf5ab905314a52bf
GeorgeZhang2035 <goerge.zhang@silksoftware.com>
2024-04-22 17:59:12 +0800

    filter of network extended

--- src/core/network/utils/index.ts
+++ src/core/network/utils/index.ts
@@ -3,6 +3,8 @@ import { Prisma } from '@prisma/client';
 export const formateFilter = ({
   creator,
   status,
+  uavs,
+  connectMap,
 }): Prisma.NetworkWhereInput[] => {
   const resultFilter = [];
   if (creator) {
@@ -27,6 +29,51 @@ export const formateFilter = ({
         AND: content.map((item: number) => ({ status: { not: item } })),
       });
   }
+  if (uavs) {
+    const { quantifier, content = [] } = uavs;
+    if (quantifier === 'is')
+      resultFilter.push({
+        OR: content.map((item: string) => {
+          if (item === 'empty') return { uavs: { none: {} } };
+        }),
+      });
+    else if (quantifier === 'isNot')
+      resultFilter.push({
+        AND: content.map((item: string) => {
+          if (item === 'empty') return { uavs: { some: {} } };
+        }),
+      });
+  }
+  if (connectMap) {
+    const { quantifier, content = [] } = connectMap;
+    if (quantifier === 'is')
+      resultFilter.push({
+        OR: content.map((item: string) => {
+          if (item === 'empty')
+            return {
+              OR: [
+                { connectMap: { equals: Prisma.AnyNull } },
+                { connectMap: { equals: [''] } },
+              ],
+            };
+          else return {};
+        }),
+      });
+    else if (quantifier === 'isNot')
+      resultFilter.push({
+        AND: content.map((item: string) => {
+          if (item === 'empty')
+            return {
+              AND: [
+                { connectMap: { not: Prisma.AnyNull } },
+                { connectMap: { not: { equals: [''] } } },
+              ],
+            };
+          else return {};
+        }),
+      });
+  }
+
   return resultFilter;
 };
 
